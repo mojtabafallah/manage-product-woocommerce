ba87812368fe4a17bace2938b8fbc54c4cb0f738
mojtaba fallah <mojtaba.fallah1373@gmail.com>
2021-01-20 17:18:52 +0330

    first commit

---
+++ manage-wc.php
@@ -0,0 +1,12 @@
+
+<?php
+/**
+* Plugin Name: Manage Product Woocomerce
+* Plugin URI:  https://github.com/valendesigns/option-tree/
+* Description: Theme Options UI Builder for WordPress. A simple way to create & save Theme Options and Meta Boxes for free or premium themes.
+* Version:     2.6.0
+* Author:      Derek Herman
+* Author URI:  http://valendesigns.com
+* License:     GPLv3
+* Text Domain: option-tree
+*/
